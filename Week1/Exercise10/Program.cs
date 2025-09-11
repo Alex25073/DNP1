a0e56231f5aa107dde7c9a78967194da102177f2
Alex25073 <alexsolomca169@gmail.com>
2025-09-11 22:06:09 +0200

    Added Exercise 10 and 11

---
+++ Week1/Exercise10/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise10
+{
+    class Program
+    {
+        static int bigDiff(int[] arr)
+        {
+            int max = arr[0];
+            int min = arr[0];
+
+            foreach (int num in arr)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+
+            return max - min;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 3, 5, 6 };
+            int result = bigDiff(arr);
+            System.Console.WriteLine(result); 
+        }
+    }
+}
---
+++ Week1/Exercise11/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercise11
+{
+    class Program
+    {
+        static int countClumps(int[] nums)
+        {
+            int clumps = 0;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        clumps++;
+                        i = j;
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            return clumps;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 2, 3, 4, 4 };
+            int result = countClumps(nums);
+            System.Console.WriteLine(result);
+        }
+    }
+}
